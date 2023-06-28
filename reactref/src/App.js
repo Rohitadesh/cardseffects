14ff9b35070918b44f00f1b8e26ed0cb9be70d2c
Rohitadesh <adeshnagalikar2000@gmail.com>
2023-06-28 08:48:04 +0530

    inital commit

---
+++ reactref/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import {Stopwatch} from './watch.js';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
