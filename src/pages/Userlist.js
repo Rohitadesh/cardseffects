9de4b518868b8a4d1d9d5d35c3c0356de39595c8
Rohitadesh <adeshnagalikar2000@gmail.com>
2024-04-10 13:43:46 +0530

    data commit

--- src/pages/Userlist.js
+++ src/pages/Userlist.js
@@ -12,13 +12,15 @@ const Userlist= () =>{
    const sportsBall = [whiteball,baseball,redball,tball,volleyball,football,batname,basketball]
     const price=["350","250","400","500","150","300","920","1500"]
     return(
-        <div className="h-screen w-screen border border-1 border-red-300 flex flex-col justify-center items-center bg-slate-800">
-            <div className="h-[98%] w-[60%] border border-black shadow-inner rounded-md flex flex-col justify-center items-center flex-wrap gap-3 ">
+        <div className="h-screen w-screen flex flex-col justify-center items-center ">
+            <div className="h-[98%] w-[60%] shadow-inner rounded-md flex flex-col justify-center items-center flex-wrap gap-3 
+            sm:max-lg:flex-wrap sm:max-lg:w-[80%]">
                 {sportsBall.map((e,index)=>{
                     return(
-                        <div key={index} className="h-[47%] border border-green-500 w-[20%] rounded-md shadow-md flex flex-col items-center justify-center gap-8">
+                        <div key={index} className="h-[47%] glass border border-green-500 w-[20%] rounded-md shadow-md flex flex-col items-center justify-center gap-8
+                        sm:max-lg:w-[25%] sm:max-lg:">
                             <Image src={e}  className="w-[40%] h-[25%]"/>
-                            <h1 className="text-xl text-white text-center flex gap-2">Price: {price[index]} Rs</h1>
+                            <h1 className="text-xl text-white text-center flex gap-2 sm:max-lg:text-md">Price: {price[index]} Rs</h1>
                         </div>
                         
                     )
